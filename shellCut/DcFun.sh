f3a1c8e026070da61c1e908c0e4d394b82ee8960
Lander Dai <dclarken@163.com>
2019-09-23 10:12:30 +0800

    Create DcFun.sh

---
+++ shellCut/DcFun.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+dateNow=`date +"%Y-%m-%d"`
+timeNow=`date +%s%N`
+binPath=`pwd`
+outFile="${binPath}/.out.log"
+outDir="${binPath}/logs"
+
+function touchFile()
+{
+	[[ -f $1 ]] && (cat /dev/null > $1; echo "Script initialization: $1 exit clear it") || (touch $1; echo "Script initialization: $1 not exit touch it")
+}
+#touchFile ${outFile}
+function touchDir()
+{
+	[[  -d "$1" ]] && (echo "Script initialization: $1 exit" ) || (mkdir -p ${1%*/};echo "Script initialization: $1 not exit mkdir it")
+}
+#touchDir ${outDir}
+function logOut()
+{
+        if [[ -z $2 ]];then echo -e "$@" | tee -a "${outFile}"
+        elif [[ -z $3 ]];then echo -e "$1" | tee -a "$2"
+        elif [[ $3 == 'withTime' ]];then echo -e "$(date '+%Y-%m-%d %H:%M:%S.888')|$1" | tee -a "$2"
+        fi
+}
+#logOut "123"
+#logOut "123" ${outFile}
+#logOut "123" ${outFile} withTime
+function varChk()
+{
+        num=`echo $1| sed  s/number=//g`
+        echo "Number of variables: $1"
+        let num++
+        if [[ $num -eq $# ]];then 
+                echo "varChk success, the number of variables is sufficient";
+        else
+                echo "varChk failed, The number of variables is not enough";
+                exit 1
+        fi
+}
+# varChk number=3 var1 var2 var3
+
+function telnetChk()
+{
+        ip=$1
+        port=$2
+        result=`sleep 1|telnet $ip $port`
+        flag=`echo -e "${result}" | grep 'Escape character is'|sed 's/is.*$/is/g'`
+        if [ "$flag" != "Escape character is" ]; then
+                echo "telnet $ip $port failed" 
+				return 1
+        else
+                echo "telnet $ip $port success"
+                return 0
+        fi
+}
