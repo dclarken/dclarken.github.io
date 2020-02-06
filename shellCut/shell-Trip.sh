f088ceedd636a4cd79a0fb7d10bc01351fa030c2
Lander Dai <dclarken@163.com>
2020-02-06 15:00:48 +0800

    Update shell-Trip.sh

--- shellCut/shell-Trip.sh
+++ shellCut/shell-Trip.sh
@@ -2,11 +2,11 @@
 ssh user@ip "cmd"
 #父目录
 echo $(dirname $(pwd))
-============================================================================================================
+#============================================================================================================
 #!/bin/bash
 cd `dirname $0`
 binPath=`pwd`
-============================================================================================================
+#============================================================================================================
 #文本操作
 #!/bin/bash
 sed -i 's/ /\n/g' ${filename}
@@ -38,7 +38,11 @@ grep -Po
 echo "Hello, my name is aming."|grep -Po '(?<=Hello, ).*(?= aming.)'  -> my name is
 #awk函数
 awk 'BEGIN {print"begin"} {print $1} END {print"END"}'
-============================================================================================================
+#假设 s 是含回车的字符串，将换行转化为tab
+echo $s|tr '\n' '\t'
+#log日志查找，查找目录下的所有文件，color参数添加颜色标记
+grep -ir --color findChar *normal* .
+#============================================================================================================
 #创建用户，（制作免密）
 usergroup=group
 username=name 
@@ -56,109 +60,16 @@ ssh-keygen -t rsa
 userdel -r username
 #添加用户到组
 gpasswd -a username ${usergroup}
-============================================================================================================
+#============================================================================================================
 #跳转机器 到其他服务器的免密
 cat ~/.ssh/id_rsa.pub_跳转机器ip
 复制到需要免密服务器的 ~/.ssh/authorized_keys
-============================================================================================================
+#============================================================================================================
 #制作具有root权限的其他用户
 groupadd -g 333 ywadmin
 useradd -g ywadmin -d /app/ywadmin/ -u 333 ywadmin
 echo 'ywadmin ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoer
-============================================================================================================
-#远程同登陆 有的时候直接远程同步会失败，在前面加一层远程登陆又好了
-#!/bin/bash
-##自动填写密码进行同步
-acount=user@ip
-passwd=Passwd
-destPath=
-destPathTemp=
-#acount=$1 destPath=$2 
-ssh_expect(){
-	/usr/bin/expect <<-EOF
-	set time 30
-	spawn ssh $1
-	expect {
-		"*yes/no" { send "yes\r"; exp_continue }
-		"*password:" { send "${passwd}\r" }
-	}
-	expect "*@*"
-	EOF
-}
-ssh_expect ${acount}@${ip}
-============================================================================================================
-#远程同步
-#!/bin/bash
-##自动填写密码进行同步
-acount=user@ip
-passwd=Passwd
-destPath=
-destPathTemp=
-#acount=$1 destPath=$2 
-rsync_expect(){
-	/usr/bin/expect <<-EOF
-	set time 30
-	spawn rsync -avH $1 $2
-	expect {
-		"*yes/no" { send "yes\r"; exp_continue }
-		"*password:" { send "${passwd}\r" }
-	}
-	expect "*@*"
-	EOF
-}
-##同步远程服务器至临时目标目录,可执行命令rsync_expect
-rsync_expect ${acount}:${destPath} ${destPathTemp%/*}
-============================================================================================================
-#远程自动创建目录
-acount=name@ip
-passwd=Passwd
-destPath=
-mkdir_expect(){
-	/usr/bin/expect <<-EOF
-	set time 30
-	
-	spawn ssh $1
-	expect {
-		"*yes/no" { send "yes\r"; exp_continue }
-		"*password:" { send "${passwd}\r" }
-	}
-	expect "*@*"
-	send "mkdir -p $2\r"
-	
-	expect "*@*"
-	send "\r"
-	expect "*@*"
-	EOF
-}
-mkdir_expect ${acount} ${destPath%/*}
-============================================================================================================
-#远程调整权限
-acount=name@ip
-passwd=123456
-destPath=
-permission=name:name:644
-Ops=rsyOps
-chown_expect(){
-	/usr/bin/expect <<-EOF
-	set time 30
-	
-	spawn ssh $1
-	expect {
-		"*yes/no" { send "yes\r"; exp_continue }
-		"*password:" { send "${passwd}\r" }
-	}
-	expect "*@*"
-	send "chown -R ${3%:*} $2\r"
-	expect "*@*"
-	send "chmod -R ${3##*:} $2\r"
-	
-	expect "*@*"
-	send "\r"
-	expect "*@*"
-	EOF
-}
-chown_expect ${acount} ${destPath} ${permission}
-============================================================================================================
+#============================================================================================================
 #比较时间
 #!/bin/bash
 #时间化作秒来与当前时间作比较
@@ -177,38 +88,34 @@ do
       		#sendmail
 	fi
 done < ${filename}
-============================================================================================================
+#============================================================================================================
 #find查找精确时间生产的文件 查找建立以下指定时间内建立的文件的文件名，| xargs 可以根据实际情况补充
 find . -name "RE" -newerct '2018-12-17 02:59:00' ! -newerct '2018-12-17 03:59:00' | xargs | awk -F " " '{print $NF}'
 #文件层数n，这个参数一般放在-name前面
 -maxdepth n
 #查找到某个文件名然后替换其中某个字段，可以用修改配置文件
 find /path -name "filename" | xargs | sed -i 's///g'
-============================================================================================================
+#查找一个月前的日志并删除
+find /path -mtime +30 -type f -name \*.zip -exec rm -f {} \;
+#============================================================================================================
 #path路径变量
 path="/app/home/local"
 out="${path}/out.txt"
-============================================================================================================
-#判断字符是否为空 文件是否有内容
-STRING=
-filename=
-if [ -s "$filename" ]; then 
-    echo "filename is not empty" 
-fi
-[[ ! -s "$filename" ]] && (echo "$filename is empty";exit 1)
-#
-if [ -z "$STRING" ]; then 
-    echo "STRING is empty" 
-fi
-#-n该方法只能判断是否存在，不能判断是否为空值
-if [ -n "$STRING" ]; then 
-    echo "STRING is not empty" 
-fi
-#返回非0即退出
-[[ $? -ne 0 ]]&&exit1
-#返回0即退出
-[[ $? -eg 0 ]]&&exit1
-============================================================================================================
+#============================================================================================================
+function sql()
+{
+host=
+user=
+port=
+pass=
+query=`mysql -N -e -h$host -u$user -p$pass -P$port --default-character-set=utf8<<EOF
+use ;
+select $1 $2;
+exit
+EOF`
+echo -e "${query}"
+}
+#============================================================================================================
 dirA=/app/image/upload/fermsupload/st1/
 dirB=/app/esi/cmp-ferms-resource/image/
 
@@ -266,18 +173,50 @@ gpasswd -a ${userName} nfsnobody
 取消挂载：
 umount dirB 
 yum erase nfs-utils
-============================================================================================================
-function sql()
-{
-host=
-user=
-port=
-pass=
-query=`mysql -N -e -h$host -u$user -p$pass -P$port --default-character-set=utf8<<EOF
-use ;
-select $1 $2;
-exit
-EOF`
-echo -e "${query}"
-}
-============================================================================================================
+#============================================================================================================
+进程放在后台跑的方法
+1、ctrl+z 先放到后台 
+[1]+  Stopped                 ./test.sh
+2、bg %1   放入到后台添加&
+3、disown -h %1  使后台进程1忽略HUP信号
+4、现在执行logout或者直接关闭ssh客户端进程也会在后台继续执行了
+5、使用screen,进入后按Ctrl+A  D退出
+6、一个终端使用screen -S hello,另外一个使用 screen -x hello 可以看到相同屏幕，并同时可以操作
+#===========================================================================================
+#curl http返回码
+curl -l -m 10 -o /dev/null -s -w %{http_code} https://www.baidu.com
+#squid代理验证,代理端口为accept=127.0.0.1:3888  验证：
+curl -x "127.0.0.1:3888" -l -m 10 -o /dev/null -s -w %{http_code} https://cvm.tencentcloudapi.com
+#https url验证
+wget --no-check-certificate 
+curl -k
+#===========================================================================================================================
+#异常分析
+#TCP连接状态分析https://blog.csdn.net/m0_37556444/article/details/83000553
+netstat -nat |awk '{print $6}'|sort|uniq -c|sort -rn 
+netstat -n | awk '/^tcp/ {++S[$NF]} END {for(a in S) print a, S[a]}'
+#网络路由分析工具
+mtr -i 0.1 -n -c 100 IP或域名
+mtr --report -c 100 -n IP
+#指定间隔保存抓包
+tcpdump -i eth0 host xxxx -s0 -G 600  -w %Y_%m%d_%H%M_%S.pcap
+tcpdump host XXXX -w /tmp/%Y_%m%d_%H%M_%S.pcap 
+#curl
+curl -L --output /dev/null --silent --show-error --write-out 'lookup: %{time_namelookup}\nconnect:%{time_connect}\nappconnect:%{time_appconnect}\npretransfer:%{time_pretransfer}\nredirect:%{time_redirect}\nstarttransfer: %{time_starttransfer}\ntotal:%{time_total}\n' '58.0.61.4'
+#===========================================================================================
+#做进程内存分析：	
+jstat -gcutil pid 2000 10 #每2秒打印一条GC信息
+1)jmap -dump:live,format=b,file=xxx.xxx PID 
+2)jmap -histo:live PID > xxx.xxx 
+	 
+jstat -gccapacity pid
+jstat -class pid
+jstat -printcompilation pid
+
+top -Hp ${PID}
+stack ${PID} |grep ${TID} -A 30 
+jstat -gcunit ${PID} 1000 
+	
+top -Hp ${PID}  #得到异常的TID
+x 选中 b光标 shift+>左右移动光标
+jstack ${TID}  #搜索异常TID的16进制
